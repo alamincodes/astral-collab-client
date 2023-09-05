3ae5c821c235d71fe370668c9eefac3af8d08cf5
Srejon Khan <srejonkhan79@gmail.com>
2023-09-05 12:32:38 +0600

    feat: add lottie space themed animation

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useLottie } from "lottie-react";
+import SpaceLoaderAnim from "./SpaceLoader.json";
 
 const Loader = () => {
+  const options = {
+    animationData: SpaceLoaderAnim,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
   return (
-    <div className="flex justify-center items-center h-[300px]">
-      <div className="planet"></div>
+    <div className="flex justify-center items-center h-[500px]">
+      <div className="w-[300px]">{View}</div>
+      {/* <div className="planet"></div> */}
     </div>
   );
 };
